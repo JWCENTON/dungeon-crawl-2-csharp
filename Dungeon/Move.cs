187125b6f67b2410e1cbdca5a11675041dd80452
Amidass <aa.wrozyna@gmail.com>
2022-10-24 14:47:02 +0200

    add new function Player

--- Dungeon/Move.cs
+++ Dungeon/Move.cs
@@ -2,5 +2,33 @@
 {
     internal class Move
     {
+        internal void Player(Player player)
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            Console.WriteLine("Movement up");
+                            break;
+                        case ConsoleKey.DownArrow:
+                            Console.WriteLine("Movement down");
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            Console.WriteLine("Movement left");
+                            break;
+                        case ConsoleKey.RightArrow:
+                            Console.WriteLine("Movement right");
+                            break;
+                        case ConsoleKey.E:
+                            Console.WriteLine("Open equipment");
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
