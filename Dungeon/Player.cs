7a8452529cd320a9e4f479b85e4141b2981d107f
Amidass <aa.wrozyna@gmail.com>
2022-10-24 14:43:31 +0200

    add basic field of class

--- Dungeon/Player.cs
+++ Dungeon/Player.cs
@@ -2,5 +2,11 @@
 {
     internal class Player
     {
+        public (int, int) position { get; set; }
+        public string name { get; set; }
+        public int health { get; set; }
+        public int atack { get; set; }
+        public Equipment equipment { get; set; }
+
     }
 }
