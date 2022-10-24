fb1e880b2b57d75f66b0abc0563069b484e032b8
Amidass <aa.wrozyna@gmail.com>
2022-10-24 14:56:39 +0200

    add directory in move player function

--- Dungeon/Move.cs
+++ Dungeon/Move.cs
@@ -1,4 +1,6 @@
-﻿namespace Dungeon
+﻿using Dungeon.Enum;
+
+namespace Dungeon
 {
     internal class Move
     {
@@ -8,24 +10,35 @@
             {
                 if (Console.KeyAvailable)
                 {
+                    Direction direction;
                     ConsoleKeyInfo key = Console.ReadKey(true);
                     switch (key.Key)
                     {
                         case ConsoleKey.UpArrow:
+                            direction = Direction.Up;
                             Console.WriteLine("Movement up");
                             break;
                         case ConsoleKey.DownArrow:
+                            direction = Direction.Down;
                             Console.WriteLine("Movement down");
                             break;
                         case ConsoleKey.LeftArrow:
+                            direction = Direction.Left;
                             Console.WriteLine("Movement left");
                             break;
                         case ConsoleKey.RightArrow:
+                            direction = Direction.Right;
                             Console.WriteLine("Movement right");
                             break;
                         case ConsoleKey.E:
+                            direction = Direction.Inventory;
                             Console.WriteLine("Open equipment");
                             break;
+                        default:
+                            if (direction == Direction.Inventory)
+                            {
+
+                            }
                     }
                 }
             }
