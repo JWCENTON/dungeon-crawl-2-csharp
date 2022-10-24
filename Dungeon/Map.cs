25f3a064341606ef816735b620471bae717dfe4b
Jacek Walaszek <w.jacek99@gmail.com>
2022-10-24 15:12:13 +0200

    map and square

--- Dungeon/Map.cs
+++ Dungeon/Map.cs
@@ -1,6 +1,26 @@
 ﻿namespace Dungeon
 {
-    internal class Map
+    public class Map
     {
+        public Square[,] FullMap;
+        public int Width { get; }
+
+        public Map()
+        {
+            Width = 11;
+            FullMap = new Square[Width, Width];
+            CreateMap();
+        }
+
+        private void CreateMap()
+        {
+            for (var i = 0; i < Width; i++)
+            {
+                for (var j = 0; j < Width; j++)
+                {
+                    FullMap[i, j] = new Square(Square.SquareStatus.Empty);
+                }
+            }
+        }
     }
 }
--- Dungeon/Square.cs
+++ Dungeon/Square.cs
@@ -1,6 +1,35 @@
 ﻿namespace Dungeon
 {
-    internal class Square
+    public class Square
     {
+        private SquareStatus Status { get; set; }
+        public string SqStatus { get; set; }
+
+        public Square(SquareStatus status)
+        {
+            Status = status;
+            SqStatus = GetCharacterSquareStatus(status);
+        }
+
+        public enum SquareStatus
+        {
+            Player, Empty, Tree
+        }
+
+        public string GetCharacterSquareStatus(SquareStatus status)
+        {
+            if (status == SquareStatus.Player)
+            {
+                return "P";
+            }
+            else if (status == SquareStatus.Tree)
+            {
+                return "T";
+            }
+            else 
+            {
+                return ".";
+            }
+        }
     }
 }
