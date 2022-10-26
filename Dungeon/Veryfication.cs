75c195fb8149abef88c42c473dbb7890529d6861
Jacek Walaszek <w.jacek99@gmail.com>
2022-10-26 09:58:11 +0200

    refactored verification

--- Dungeon/Veryfication.cs
+++ Dungeon/Veryfication.cs
@@ -4,49 +4,69 @@ namespace Dungeon
 {
     public class Verification
     {
-        
+        private readonly List<string> _walls = new List<string>{"=", "|", "||"};
+
         public bool DirectionUpIsWall(Map map, Character character)
         {
-            if (map.FullMap[character.Position.x - 1, character.Position.y].GetStatus != "=" &&
-                map.FullMap[character.Position.x - 1, character.Position.y].GetStatus != "|")
+            var upDirectionStatus = map.FullMap[character.Position.x - 1, character.Position.y].GetStatus;
+            var moveUp = (character.Position.y, character.Position.x - 1);
+            foreach (var wall in _walls)
             {
-                character.Position = (character.Position.y, character.Position.x - 1);
-                return true;
+                if (upDirectionStatus == wall)
+                {
+                    return false;
+                }
             }
-            return false;
+            character.Position = moveUp;
+            return true;
         }
+
         public bool DirectionDownIsWall(Map map, Character character)
         {
-            if (map.FullMap[character.Position.x + 1, character.Position.y].GetStatus != "=" &&
-                map.FullMap[character.Position.x + 1, character.Position.y].GetStatus != "|")
+            var downDirectionStatus = map.FullMap[character.Position.x + 1, character.Position.y].GetStatus;
+            var moveDown = (character.Position.y, character.Position.x + 1);
+            foreach (var wall in _walls)
             {
-                character.Position = (character.Position.y, character.Position.x + 1);
-                return true;
+                if (downDirectionStatus == wall)
+                {
+                    return false;
+                }
             }
-            return false;
+            character.Position = moveDown;
+            return true;
         }
 
         public bool DirectionLeftIsWall(Map map, Character character)
         {
-            if (map.FullMap[character.Position.x, character.Position.y - 1].GetStatus != "||" &&
-                map.FullMap[character.Position.x, character.Position.y - 1].GetStatus != "=" &&
-                map.FullMap[character.Position.x, character.Position.y - 1].GetStatus != "|")
+            var leftDirectionStatus = map.FullMap[character.Position.x, character.Position.y - 1].GetStatus;
+            var moveLeft = (character.Position.y - 1, character.Position.x);
+            foreach (var wall in _walls)
             {
-                character.Position = (character.Position.y - 1, character.Position.x);
-                return true;
+                if (leftDirectionStatus == wall)
+                {
+                    return false;
+                }
             }
-            return false;
+            character.Position = moveLeft;
+            return true;
         }
+
         public bool DirectionRightIsWall(Map map, Character character)
         {
-            if (map.FullMap[character.Position.x, character.Position.y + 1].GetStatus != "||" &&
-                map.FullMap[character.Position.x, character.Position.y + 1].GetStatus != "=" &&
-                map.FullMap[character.Position.x, character.Position.y + 1].GetStatus != "|")
+            var rightDirectionStatus = map.FullMap[character.Position.x, character.Position.y + 1].GetStatus;
+            var moveRight = (character.Position.y + 1, character.Position.x);
+            //if (map.FullMap[character.Position.x, character.Position.y + 1].GetStatus != "||" &&
+            //    map.FullMap[character.Position.x, character.Position.y + 1].GetStatus != "=" &&
+            //    map.FullMap[character.Position.x, character.Position.y + 1].GetStatus != "|")
+            foreach (var wall in _walls)
             {
-                character.Position = (character.Position.y + 1, character.Position.x);
-                return true;
+                if (rightDirectionStatus == wall)
+                {
+                    return false;
+                }
             }
-            return false;
+            character.Position = moveRight;
+            return true;
         }
     }
 }
