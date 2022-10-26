81f59315ed60c20d71770895d30e9aa0b3b471f8
Amidass <aa.wrozyna@gmail.com>
2022-10-26 11:56:57 +0200

    Add new function Put and Take off object from map

---
+++ Dungeon/MapManager.cs
@@ -0,0 +1,17 @@
+﻿using Dungeon.Enum;
+
+namespace Dungeon
+{
+    internal class MapManager
+    {
+        public static void PutObjectOnMap(Map map, Character character)
+        {
+            map.FullMap[character.Position.x, character.Position.y] = new Square(Status.Player);
+        }
+
+        public static void TakeOffObjectFromMap(Map map, Character character)
+        {
+            map.FullMap[character.Position.x, character.Position.y] = new Square(Status.Empty);
+        }
+    }
+}
