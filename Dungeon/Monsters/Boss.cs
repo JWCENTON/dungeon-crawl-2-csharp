e68f9e17567a58914af79c55592eac2e703a2f83
Jacek Walaszek <w.jacek99@gmail.com>
2022-10-27 14:42:31 +0200

    add boss class and refactored verification

---
+++ Dungeon/Monsters/Boss.cs
@@ -0,0 +1,26 @@
+﻿using Dungeon.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Monsters
+{
+    public class Boss : Monster
+    {
+        public Boss((int, int) startedPosition)
+        {
+            Name = "BOSS";
+            //ID = count++;
+            Position = startedPosition;
+            Health = SetMonsterIntValue(21, 29);
+            AtackValue = SetMonsterIntValue(4, 8);
+            TypeAtack = "Beat";
+            this.Item = new Item("Claw");
+            IsAlive = true;
+            Type = Status.Boss;
+        }
+
+    }
+}
--- Dungeon/Veryfication.cs
+++ Dungeon/Veryfication.cs
@@ -49,40 +49,36 @@ namespace Dungeon
 
         public static bool DirectionUpIsWallOrChangeLevel(string direction, Character character)
         {
-            var upDirectionStatus = direction; //map.FullMap[character.Position.x - 1, character.Position.y].GetStatus;
             var moveUp = (character.Position.y, character.Position.x - 1);
-            LevelManager.CheckKeyAndChangeLevel(upDirectionStatus, character);
-            if (ItIsWall(upDirectionStatus, _walls)) return false;
+            LevelManager.CheckKeyAndChangeLevel(direction, character);
+            if (ItIsWall(direction, _walls)) return false;
             character.Position = moveUp;
             return true;
         }
 
         public static bool DirectionDownIsWallOrChangeLevel(string direction, Character character)
         {
-            var downDirectionStatus = direction; //map.FullMap[character.Position.x + 1, character.Position.y].GetStatus;
             var moveDown = (character.Position.y, character.Position.x + 1);
-            LevelManager.CheckKeyAndChangeLevel(downDirectionStatus, character);
-            if (ItIsWall(downDirectionStatus, _walls)) return false;
+            LevelManager.CheckKeyAndChangeLevel(direction, character);
+            if (ItIsWall(direction, _walls)) return false;
             character.Position = moveDown;
             return true;
         }
 
         public static bool DirectionLeftIsWallOrChangeLevel(string direction, Character character)
         {
-            var leftDirectionStatus = direction; //map.FullMap[character.Position.x, character.Position.y - 1].GetStatus;
             var moveLeft = (character.Position.y - 1, character.Position.x);
-            LevelManager.CheckKeyAndChangeLevel(leftDirectionStatus, character);
-            if (ItIsWall(leftDirectionStatus, _walls)) return false;
+            LevelManager.CheckKeyAndChangeLevel(direction, character);
+            if (ItIsWall(direction, _walls)) return false;
             character.Position = moveLeft;
             return true;
         }
 
         public static bool DirectionRightIsWallOrChangeLevel(string direction, Character character)
         {
-            var rightDirectionStatus = direction; //map.FullMap[character.Position.x, character.Position.y + 1].GetStatus;
             var moveRight = (character.Position.y + 1, character.Position.x);
-            LevelManager.CheckKeyAndChangeLevel(rightDirectionStatus, character);
-            if (ItIsWall(rightDirectionStatus, _walls)) return false;
+            LevelManager.CheckKeyAndChangeLevel(direction, character);
+            if (ItIsWall(direction, _walls)) return false;
             character.Position = moveRight;
             return true;
         }
