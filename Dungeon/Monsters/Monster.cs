28073ddd1c36a10789c5f60d234ddb6870f74be8
Amidass <aa.wrozyna@gmail.com>
2022-10-27 13:15:14 +0200

    change private modifier to internal for SetMonsterIntValue method

--- Dungeon/Monsters/Monster.cs
+++ Dungeon/Monsters/Monster.cs
@@ -13,7 +13,7 @@ namespace Dungeon.Monsters
         public string TypeAtack { get; set; }
         public Item Item { get; set; }
         public bool IsAlive { get; set; }
-        public int SetMonsterIntValue(int minHealth, int maxHealth)
+        internal int SetMonsterIntValue(int minHealth, int maxHealth)
         {
             Random random = new Random();
 
