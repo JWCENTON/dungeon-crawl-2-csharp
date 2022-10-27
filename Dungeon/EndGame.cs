378df5f38af22e8a38ef7ebff0214d89c769d5a3
Jacek Walaszek <w.jacek99@gmail.com>
2022-10-27 13:17:08 +0200

    endgame completed

---
+++ Dungeon/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    public class EndGame
+    {
+        public static bool CheckForEndGame(Character player)
+        {
+            if (player.Health <= 0)
+            {
+                Display.EndGame();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
