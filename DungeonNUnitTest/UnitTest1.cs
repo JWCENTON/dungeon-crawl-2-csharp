8648ac473449ba89181fd92125964f82dbdbac99
Jacek Walaszek <w.jacek99@gmail.com>
2022-11-08 11:36:27 +0100

    Nunittest project added

---
+++ DungeonNUnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DungeonNUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
