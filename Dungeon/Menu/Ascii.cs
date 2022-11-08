322917be8852432af1ed96e9121956c3b617e4e0
unknown <aa.wrozyna@gmail.com>
2022-11-08 14:02:35 +0100

    add new working class MenuManager

--- Dungeon/Menu/Ascii.cs
+++ Dungeon/Menu/Ascii.cs
@@ -8,5 +8,9 @@
                                         $"{space}2. Choose character\n" +
                                         $"{space}3. Load Game\n" +
                                         $"{space}4. Settings\n\n";
+
+        public static string selectCharacterText = "\n1) Alice"+
+                                                    "\n2) Jacek"+
+                                                    "\n3) Adam";
     }
 }
