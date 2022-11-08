76897ba4e2774d6bcd021413aae248d2ecedd9ad
Jacek Walaszek <w.jacek99@gmail.com>
2022-11-08 12:09:39 +0100

    eng game test class added

---
+++ DungeonNUnitTest/EndGameUnitTest.cs
@@ -0,0 +1,36 @@
+using Dungeon.Monsters;
+using Dungeon;
+
+namespace DungeonNUnitTest
+{
+    public class Tests
+    {
+        private (int, int) _bossPosition;
+        private Boss _boss;
+        private Alice _player;
+
+        [SetUp]
+        public void Setup()
+        {
+            _bossPosition = (44, 44);
+            _boss = new Boss(_bossPosition);
+            _player = new Alice();
+        }
+
+        [Test]
+        public static bool CheckForEndGame()
+        {
+            if (_player.Health <= 1)
+            {
+                Display.EndGameLost();
+                return false;
+            }
+            else if (_boss.Health <= 1)
+            {
+                Display.EndGameWon();
+                return false;
+            }
+            return true;
+        }
+    }
+}
