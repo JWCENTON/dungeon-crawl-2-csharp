6208f1ed33a186a9b554251038aafc3a0173282b
Jacek Walaszek <w.jacek99@gmail.com>
2022-11-09 12:14:11 +0100

    dungeonDbmanager ready to test connection to db

--- Dungeon/SqlManager/dungeonDbManager.cs
+++ Dungeon/SqlManager/dungeonDbManager.cs
@@ -1,6 +1,17 @@
-﻿namespace Dungeon.SqlManager;
+﻿using System.Configuration;
+namespace Dungeon.SqlManager;
 
 public class dungeonDbManager
 {
+    public string ConnectionString => ConfigurationManager.AppSettings["connectionString"];
+    public dungeonDbManager()
+    {
+        EnsureConnectionSuccessful();
+    }
+
+    public bool TestConnection()
+    {
+        using var connection = new SqlManager
+    }
 }
 
