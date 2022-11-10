4787963881368447147efe29ee9619ac186a0c8c
Jacek Walaszek <w.jacek99@gmail.com>
2022-11-10 08:40:00 +0100

    modified monsters db

--- Dungeon/Data/dungeonDb.sql
+++ Dungeon/Data/dungeonDb.sql
@@ -1,7 +1,7 @@
 USE [master]
 GO
 
-/****** Object:  Database [dungeonDb]    Script Date: 11/9/2022 15:31:06 ******/
+/****** Object:  Database [dungeonDb]    Script Date: 11/10/2022 08:39:14 ******/
 DROP DATABASE [dungeonDb]
 GO
 
